36666073ab40cbcc2543ecfbb7f85321d3a8d824
shazamch <shazamrazzaq1234@gmail.com>
2024-10-28 12:01:43 +0500

    Token Auth Added

--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController');
+const authenticateToken = require('../middleware/authenticateToken');
 
-router.post('/', customerController.createCustomer);
-router.get('/', customerController.getAllCustomers);
-router.get('/:id', customerController.getCustomerById);
-router.put('/updatealldata/:customerId', customerController.updateAllData);
-router.delete('/:id', customerController.deleteCustomer);
+router.post('/', authenticateToken, customerController.createCustomer);
+router.get('/', authenticateToken, customerController.getAllCustomers);
+router.get('/:id', authenticateToken, customerController.getCustomerById);
+router.put('/updatealldata/:customerId', authenticateToken, customerController.updateAllData);
+router.delete('/:id', authenticateToken, customerController.deleteCustomer);
 
 module.exports = router;
